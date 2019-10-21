6c9545ba28f56b670bb4c78c9471bcbc197d8d56
Pernatov <pernatov@mail.ru>
2019-10-21 19:14:17 +0300

    Первый урок, ветка мастер, создано 6 переменных

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Переменные:
+var money = 100;
+var income = 30;
+var addExpenses = 40;
+var deposit = 55;
+var mission = 'conquer';
+var period = 7;
+// Вывод в консоль:
+console.log('Переменная money = ' + period);
+console.log('Переменная income = ' + income);
+console.log('Переменная addExpenses = ' + addExpenses);
+console.log('Переменная deposit = ' + deposit);
+console.log('Переменная mission = ' + mission);
+console.log('Переменная period = ' + period);
