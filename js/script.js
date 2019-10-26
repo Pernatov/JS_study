d6574572abb40a2f11479ebe87c92f90c7d31409
Pernatov <pernatov@mail.ru>
2019-10-26 17:08:11 +0300

    Сделан конспект лекции №2

--- js/script.js
+++ js/script.js
@@ -1,17 +1,160 @@
-// Переменные:
-var money = 100;
-var income = 30;
-var addExpenses = 40;
-var deposit = 55;
-var mission = 'conquer';
-var period = 7;
-// Вывод в консоль:
-console.log('Переменная money = ' + period);
-console.log('Переменная income = ' + income);
-console.log('Переменная addExpenses = ' + addExpenses);
-console.log('Переменная deposit = ' + deposit);
-console.log('Переменная mission = ' + mission);
-console.log('Переменная period = ' + period);
+// УРОК 2. Типы переменных. КОНСПЕКТ УРОКА
+// let myVar;
 
-let text = 'Урок Первый'
-alert(text);
+// myVar = 10;
+// console.log('Тип данных переменной: ' + typeof myVar);
+// myVar = 'Hello world';
+// console.log('Тип данных переменной: ' + typeof myVar);
+// myVar = true;
+// console.log('Тип данных переменной: ' + typeof myVar);
+// myVar = null;
+// console.log('Тип данных переменной: ' + typeof myVar);
+// myVar = undefined;
+// console.log('Тип данных переменной: ' + typeof myVar);
+// myVar = Symbol();
+// console.log('Тип данных переменной: ' + typeof myVar);
+// myVar = {};
+// console.log('Тип данных переменной: ' + typeof myVar);
+
+// console.log('null: ', null);
+
+// let maVar;
+// console.log('myVar: ', myVar);
+
+// let obj = {};
+// console.log('obj.prop: ', obj.prop);
+
+// let arr = [1,2,3];
+// console.log('arr: ', arr);
+
+// let myTrue = true;
+// let myFalse = false;
+
+// console.log('myTrue: ', myTrue);
+// console.log('myFalse: ', myFalse);
+
+// console.log(2 == 2);
+// console.log(2 === 3);
+
+// console.log(Boolean(5));
+// console.log(Boolean('Hello'));
+// console.log(Boolean({}));
+// console.log(Boolean([]));
+// console.log(Boolean(function(){}));
+
+// let myTrue = true; 
+// let myFalse = false;
+
+// console.log(Boolean(0)); 
+// console.log(Boolean(undefined)); 
+// console.log(Boolean(null)); 
+// console.log(Boolean(NaN)); 
+// console.log(Boolean(''));
+
+// let myTrue = true; 
+// let myFalse = false;
+
+// ||- или && - и ! - не
+
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || false);
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && false);
+
+// console.log(!true);
+// console.log(!false);
+
+// console.log(50); 
+// console.log(8.5);
+// console.log(.5); 
+// console.log(5e6); 
+// console.log(0B1111); 
+// console.log(0011); 
+// console.log(0X22aacc); 
+// console.log(Infinity); 
+// console.log(NaN);
+
+// console.log(typeof 50); 
+// console.log(typeof 8.5);
+// console.log(typeof .5); 
+// console.log(typeof 5e6); 
+// console.log(typeof 0B1111); 
+// console.log(typeof 0011); 
+// console.log(typeof 0X22aacc); 
+// console.log(typeof Infinity); 
+// console.log(typeof NaN);
+
+// console.log(3 + 4); 
+// console.log(4 - 3); 
+// console.log(2 * 3); 
+// console.log(9 / 2); 
+// console.log(9 % 2);
+
+// let n = 15; 
+// console.log('n: ', n);
+// // n = n + 3;
+// n += 3;
+// console.log('n: ', n);
+// // n = n - 6;
+// n -= 6;
+// console.log('n: ', n);
+// // n = n / 3;
+// n /= 3;
+// console.log('n: ', n);
+
+// console.log(3 > 2); 
+// console.log(3 < 2); 
+// console.log(5 >= 3); 
+// console.log(10 <= 5); 
+// console.log(5 === 5); 
+// console.log(5 !== 6); 
+// console.log(5 == '5');
+
+// console.log(Math.sqrt(25));
+// console.log(Math.pow(5, 3));
+// Найти новый оператор возведения в степень (ДЗ)
+// console.log(Number.isInteger(5));
+// console.log(Number.isInteger(5.5));
+
+// let myString = 'Hello world!'; 
+// let myStr = "\t Hello \"my\" \n friends!";
+// console.log('Hello "my" friends!');
+// console.log('myStr: ', myStr);
+// console.log('Hello' + ' ' + 'world');
+// console.log(5 + '10');
+// console.log(typeof (5 + '10'));
+// let str = 'Hello my friends!'; 
+// console.log(myString + ' ' + str);
+
+// let str = 'Hello my Friends!';
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.charAt(0));
+// console.log(str.charAt(1));
+// console.log(str.charAt(2));
+// console.log(str.charAt(3));
+// console.log(str.charAt(4));
+// console.log(str[0]);
+
+// console.log(str.substring(6)); 
+// console.log(str.substring(9, 15)); 
+// console.log(str.slice(6)); 
+// console.log(str.slice(-8)); 
+// console.log(str.substr(6, 9));
+
+// console.log(str.indexOf('Friends'));
+// console.log(str.indexOf('d'));
+// console.log(str.indexOf('a'));
+
+// console.log(str.replace('my Friends', 'World'));
+
+// console.log(str.split(' '));
+
+// let str = 'apple, kiwi, orange';
+// console.log(str.split(', '));
+
+// ДОМАШНЕЕ ЗАДАНИЕ:
